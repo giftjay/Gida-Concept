97d2a15192dcd5682481be91cff5b83012758dad
giftjay <67248884+giftjay@users.noreply.github.com>
2022-05-04 20:22:47 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,78 @@
+
+<!Doctype html>
+<html lang="en">
+
+<!--Head start-->
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=devicewidth,initial-scale=1.0">
+<link rel="stylesheet" href="gida concept.css">
+<link rel="shortcut icon" href="images/logo-1.jpg" type="images/x-icon"/>
+</head>
+<!--head end-->
+
+<body>
+<!--header page-->
+
+<section class="header-tab">
+<div class="header-img">
+<img class="header-img-tab" src="images/logo-1.jpg"></img>
+</div>
+
+
+
+
+<div class="header-img-1">
+<img class="header-img-tab" src="images/logo-1.jpg"></img>
+</div>
+
+
+<div class="header-text-1">
+<p>Menu </p>
+    <div class="sub-menu-1">
+    <ul>
+    <li> <a href="#Services">Services</a></li>
+    <li> <a href="#Contact">Contact</a></li>
+    <li><a href="#About">About</a></li>
+    <li> <a href="#">Tutorials </a></li>
+    
+    </ul>
+    </div>
+    </ul>
+
+</div>
+
+
+<div class="header-text">
+<p> <a href="#Services">Services</a></p>
+</div>
+
+
+<div class="header-text">
+<p><a href="#Contact">Contact</a></p>
+</div>
+
+<div class="header-text">
+<p><a href="#About">About</a></p>
+</div>
+
+<div class="header-text">
+<p><a href="#">Tutorials </a></p>
+</div>
+
+
+</section>
+
+
+<!--header page end-->
+
+
+
+<body>
+
+<footer>
+
+
+</footer>
+
+</html>
